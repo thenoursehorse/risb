7e1a4e140c2bf85c77fd2dc5e126dc9f0870faa2
H. L. Nourse <32994022+thenoursehorse@users.noreply.github.com>
2023-09-19 17:19:10 +0900

    change to pytest

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,126 +1,87 @@
 #!/usr/bin/env python
 
 import numpy as np
-import unittest
+from pytest import approx
 from common import build_cubic_h0_k, build_block_mf_matrices
 from triqs.operators import *
 from risb import helpers
 from risb.kweight import SmearingKWeight
-from risb.embedding import EmbeddingAtomDiag
 
 beta = 10
 gf_struct=[('up',2),('dn',2)]
 
-class tests(unittest.TestCase):
-
-    # FIXME eig, vec to h5 and load ref to test
-    def test_get_hqp(self):
-        h0_k = build_cubic_h0_k(gf_struct=gf_struct)
-        R, Lambda = build_block_mf_matrices(gf_struct=gf_struct)
-        for block,_ in gf_struct:
-            eig, vec = helpers.get_h_qp(R[block], Lambda[block], h0_k[block])
+# FIXME eig, vec to h5 and load ref to test
+def test_get_hqp(subtests):
+    h0_k = build_cubic_h0_k(gf_struct=gf_struct)
+    R, Lambda = build_block_mf_matrices(gf_struct=gf_struct)
+    for block,_ in gf_struct:
+        eig, vec = helpers.get_h_qp(R[block], Lambda[block], h0_k[block])
+    #with subtests.test(msg="eigenvalues"):
+    #    for bl,_ in gf_struct:
+    #        assert eig[bl] == approx(eig_expected, abs=1e-8)
+    #with subtests.test(msg="eigenvectors"):
+    #    for bl,_ in gf_struct:
+    #        assert vec[bl] == approx(vec_expected, abs=1e-8)
         
-    # FIXME eig, R, vec, and wks in h5 and load ref to test
-    def test_get_ke(self):
-        h0_k = build_cubic_h0_k(gf_struct=gf_struct)
-        R, Lambda = build_block_mf_matrices(gf_struct=gf_struct)
-        for block,_ in gf_struct:
-            np.fill_diagonal(Lambda[block], 0.5)
-        eig = dict()
-        vec = dict()
-        for block,_ in gf_struct:
-            eig[block], vec[block] = helpers.get_h_qp(R[block], Lambda[block], h0_k[block])
-        kweight = SmearingKWeight(beta=beta, mu=0)
-        wks = kweight.update_weights(eig)
-        h0_R = dict()
-        ke = dict()
-        for block,_ in gf_struct:
-            h0_R[block] = helpers.get_h0_R(R[block], h0_k[block], vec[block])
-            ke[block] = helpers.get_ke(h0_R[block], vec[block], wks[block])
+# FIXME eig, R, vec, and wks in h5 and load ref to test
+def test_get_ke():
+    h0_k = build_cubic_h0_k(gf_struct=gf_struct)
+    R, Lambda = build_block_mf_matrices(gf_struct=gf_struct)
+    for bl,_ in gf_struct:
+        np.fill_diagonal(Lambda[bl], 0.5)
+    eig = dict()
+    vec = dict()
+    for bl,_ in gf_struct:
+        eig[bl], vec[bl] = helpers.get_h_qp(R[bl], Lambda[bl], h0_k[bl])
+    kweight = SmearingKWeight(beta=beta, mu=0)
+    wks = kweight.update_weights(eig)
+    h0_R = dict()
+    ke = dict()
+    for block,_ in gf_struct:
+        h0_R[bl] = helpers.get_h0_R(R[bl], h0_k[bl], vec[bl])
+        ke[bl] = helpers.get_ke(h0_R[bl], vec[bl], wks[bl])
+    #for bl,_ in gf_struct:
+    #    assert ke[bl] == approx(ke_expected, abs=1e-8)
         
-    # FIXME vec and wks in h5 and load ref to test
-    def test_get_pdensity(self):
-        h0_k = build_cubic_h0_k(gf_struct=gf_struct)
-        R, Lambda = build_block_mf_matrices(gf_struct=gf_struct)
-        for block,_ in gf_struct:
-            np.fill_diagonal(Lambda[block], 0.5)
-        eig = dict()
-        vec = dict()
-        for block,_ in gf_struct:
-            eig[block], vec[block] = helpers.get_h_qp(R[block], Lambda[block], h0_k[block])
-        kweight = SmearingKWeight(beta=beta, mu=0)
-        wks = kweight.update_weights(eig)
-        pdensity = dict()
-        for block,_ in gf_struct:
-            pdensity[block] = helpers.get_pdensity(vec[block], wks[block])
+# FIXME vec and wks in h5 and load ref to test
+def test_get_pdensity():
+    h0_k = build_cubic_h0_k(gf_struct=gf_struct)
+    R, Lambda = build_block_mf_matrices(gf_struct=gf_struct)
+    for bl,_ in gf_struct:
+        np.fill_diagonal(Lambda[bl], 0.5)
+    eig = dict()
+    vec = dict()
+    for bl,_ in gf_struct:
+        eig[bl], vec[bl] = helpers.get_h_qp(R[bl], Lambda[bl], h0_k[bl])
+    kweight = SmearingKWeight(beta=beta, mu=0)
+    wks = kweight.update_weights(eig)
+    pdensity = dict()
+    for bl,_ in gf_struct:
+        pdensity[bl] = helpers.get_pdensity(vec[bl], wks[bl])
+    #for bl,_ in gf_struct:
+    #    assert pdensity[bl] == approx(pdensity_expected, abs=1e-8)
 
-    def test_get_d(self):
-        pdensity = {'up': np.array([[0.19618454, 0.        ],
-                                    [0.        , 0.19618454]]), 
-                    'dn': np.array([[0.19618454, 0.        ],
-                                    [0.        , 0.19618454]])}
-        ke = {'up': np.array([[-0.13447044,  0.        ],
-                              [ 0.        , -0.13447044]]),
-              'dn': np.array([[-0.13447044,  0.        ],
-                              [ 0.        , -0.13447044]])}
-        D = dict()
-        for block,_ in gf_struct:
-            D[block] = helpers.get_d(pdensity[block], ke[block])
-        D_expected = {'up': np.array([[-0.33862285,  0.        ],
-                                      [ 0.        , -0.33862285]]), 
-                      'dn': np.array([[-0.33862285,  0.       ],
-                                      [ 0.        , -0.33862285 ]])}
-        for block,_ in gf_struct:
-            np.testing.assert_allclose(D_expected[block], D[block], rtol=0, atol=1e-8)
+def test_get_d():
+    pdensity = np.array([[0.19618454, 0.        ],
+                         [0.        , 0.19618454]])
+    ke = np.array([[-0.13447044,  0.        ],
+                   [ 0.        , -0.13447044]])
+    D = helpers.get_d(pdensity, ke)
+    D_expected = np.array([[-0.33862285,  0.        ],
+                           [ 0.        , -0.33862285]])
+    assert D == approx(D_expected, abs=1e-8)
         
-    def test_get_lambda_c(self): 
-        Lambda = {'up': np.array([[0.5, 0. ],
-                                  [ 0., 0.5]]), 
-                  'dn': np.array([[0.5, 0.  ],
-                                  [ 0., 0.5]])}
-        R = {'up': np.array([[1., 0. ],
-                             [0., 1.]]), 
-             'dn': np.array([[1., 0.  ],
-                             [0., 1.]])}
-        pdensity = {'up': np.array([[0.19618454, 0.        ],
-                                    [0.        , 0.19618454]]), 
-                    'dn': np.array([[0.19618454, 0.        ],
-                                    [0.        , 0.19618454]])}
-        D = {'up': np.array([[-0.33862285,  0.        ],
-                             [ 0.        , -0.33862285]]), 
-             'dn': np.array([[-0.33862285,  0.       ],
-                             [ 0.        , -0.33862285 ]])}
-        Lambda_c = dict()
-        for block,_ in gf_struct:
-            Lambda_c[block] = helpers.get_lambda_c(pdensity[block], R[block], Lambda[block], D[block])
-        Lambda_c_expected = {'up': np.array([[ 0.01813814, -0.        ],
-                                             [-0.        ,  0.01813814]]),
-                             'dn': np.array([[ 0.01813814, -0.        ],
-                                             [-0.        ,  0.01813814]])}
-        for block,_ in gf_struct:
-            np.testing.assert_allclose(Lambda_c_expected[block], Lambda_c[block], rtol=0, atol=1e-8)
-
-    # FIXME nf, mcf, and nc in h5 and load ref to test
-    def test_solve_emb(self):
-        Lambda_c = {'up': np.array([[ 0.01813814, -0.        ],
-                                    [-0.        ,  0.01813814]]),
-                    'dn': np.array([[ 0.01813814, -0.        ],
-                                    [-0.        ,  0.01813814]])}
-        D = {'up': np.array([[-0.33862285,  0.        ],
-                             [ 0.        , -0.33862285]]), 
-             'dn': np.array([[-0.33862285,  0.       ],
-                             [ 0.        , -0.33862285 ]])}
-        h_loc = n('up',0) * n('dn',0)
-        embedding = EmbeddingAtomDiag(h_loc, gf_struct) 
-        embedding.set_h_emb(Lambda_c, D)
-        embedding.solve()
-        Nf = dict()
-        Nc = dict()
-        Mcf = dict()
-        for bl, bl_size in gf_struct:
-            Nf[bl] = embedding.get_nf(bl)
-            Nc[bl] = embedding.get_nc(bl)
-            Mcf[bl] = embedding.get_mcf(bl)
-
-if __name__ == '__main__':
-    unittest.main()
+def test_get_lambda_c(): 
+    Lambda = np.array([[0.5, 0. ],
+                       [ 0., 0.5]])
+    R = np.array([[1., 0. ],
+                  [0., 1.]])
+    pdensity = np.array([[0.19618454, 0.        ],
+                         [0.        , 0.19618454]])
+    D = np.array([[-0.33862285,  0.        ],
+                  [ 0.        , -0.33862285]])
+    Lambda_c = dict()
+    Lambda_c = helpers.get_lambda_c(pdensity, R, Lambda, D)
+    Lambda_c_expected = np.array([[ 0.01813814, -0.        ],
+                                  [-0.        ,  0.01813814]])
+    assert Lambda_c == approx(Lambda_c_expected, abs=1e-8)
