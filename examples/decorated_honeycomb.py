4e5c04cd3f182ce5fba8df275c0e41ce6ae40d80
H. L. Nourse <32994022+thenoursehorse@users.noreply.github.com>
2023-10-02 17:51:57 +0900

    use gf_struct blocks

--- examples/decorated_honeycomb.py
+++ examples/decorated_honeycomb.py
@@ -12,7 +12,7 @@ from risb.embedding import EmbeddingAtomDiag, EmbeddingDummy
 from risb.helpers import get_h0_kin_k, block_to_full
 from risb.helpers_triqs import get_h0_loc
 
-def get_h0_k(tg=0.5, tk=1.0, nkx=18, spin_names=['up','dn']):
+def get_h0_k(tg=0.5, tk=1.0, nkx=18, spin_names=['up','dn'], basis='trimer'):
     na = 2 # Break up unit cell into 2 clusters
     n_orb = 3 # Number of orbitals/sites per cluster
     phi = 2.0 * np.pi / 3.0 # bloch factor for transforming to trimer orbital basis
@@ -36,6 +36,7 @@ def get_h0_k(tg=0.5, tk=1.0, nkx=18, spin_names=['up','dn']):
     d0 = ( 1.0, 0.0 )
     d1 = ( -0.5, np.sqrt(3.0)/2.0 )
     d2 = ( -0.5, -np.sqrt(3.0)/2.0 )
+    d_vec = [d0, d1, d2]
         
     h0_k = np.zeros([n_k, na, na, n_orb, n_orb], dtype=complex)
 
@@ -45,99 +46,112 @@ def get_h0_k(tg=0.5, tk=1.0, nkx=18, spin_names=['up','dn']):
 
         # Dispersion terms between clusters
         if (i == 0) and (j == 1):
-            h0_k[k,i,j,m,mm] = -(tg/3.0) * ( np.exp(1j * np.dot(kay,d0)) 
-                               + np.exp(1j * np.dot(kay,d1)) * np.exp(1j * phi * (mm-m)) 
-                               + np.exp(1j * (np.dot(kay,d2)))*np.exp(1j * 2.0 * phi * (mm-m)) )
+            for a in range(n_orb):
+                h0_k[k,i,j,m,mm] += - (tg/3.0) * np.exp(1j * kay @ d_vec[a]) * np.exp(1j * phi * (mm-m) * a)
         elif (i == 1) and (j == 0):
-            h0_k[k,i,j,m,mm] = -(tg/3.0) * ( np.exp(-1j * np.dot(kay,d0)) 
-                               + np.exp(-1j * np.dot(kay,d1)) * np.exp(-1j *phi * (m-mm)) 
-                               + np.exp(-1j * np.dot(kay,d2)) * np.exp(-1j * 2.0 * phi * (m-mm)) )
-            
+            for a in range(n_orb):
+                h0_k[k,i,j,m,mm] += - (tg/3.0) * np.exp(-1j * kay @ d_vec[a]) * np.exp(-1j * phi * (m-mm) * a)
         # Local terms on a cluster
         elif (i == j) and (m == mm):
-            if m == 0:
-                h0_k[k,i,j,m,mm] = - 2.0 * tk
-            else:
-                h0_k[k,i,j,m,mm] = tk
-
+                h0_k[k,i,j,m,mm] = -2.0 * tk * np.cos(m * phi)
         else:
             continue
-
+        
     # Get rid of the inequivalent block structure
     h0_k_out = dict()
     for bl in spin_names:
         h0_k_out[bl] = block_to_full( h0_k )
     return h0_k_out
 
-def get_hubb_N(spin_names, U=0, tk=0, n_orb=3):
+def get_hubb_trimer(spin_names, U=0, tk=0):
+    n_orb = 3
     phi = 2.0 * np.pi / n_orb # bloch factor for transforming to molecular orbital basis
-
+    block_map = {0:'A', 1:'E1', 2:'E2'}
+    orb_map = {0:0, 1:0, 2:0}
+    def get_c(s, m, dagger):
+        if dagger:
+            return c_dag(s + "_" + block_map[m], orb_map[m])
+        else:
+            return c(s + "_" + block_map[m], orb_map[m])
     spin_up = spin_names[0]
     spin_dn = spin_names[1]
     
     h_loc = Operator()
     for a,m,mm,s in product(range(n_orb), range(n_orb), range(n_orb), spin_names):
-        h_loc += (-tk / float(n_orb)) * c_dag(s,m) * c(s,mm) * np.exp(-1j * phi * a * m) * np.exp(1j * phi * np.mod(a+1,n_orb) * mm)
-        h_loc += (-tk / float(n_orb)) * c_dag(s,m) * c(s,mm) * np.exp(-1j * phi * np.mod(a+1,n_orb) * m) * np.exp(1j * phi * a * mm)
-    
+        h_loc += (-tk / float(n_orb)) * get_c(s, m, True) * get_c(s, mm, False) * np.exp(-1j * phi * a * m) * np.exp(1j * phi * np.mod(a+1,n_orb) * mm)
+        h_loc += (-tk / float(n_orb)) * get_c(s, m, True) * get_c(s, mm, False) * np.exp(-1j * phi * np.mod(a+1,n_orb) * m) * np.exp(1j * phi * a * mm)
+
     for m,mm,mmm in product(range(n_orb), range(n_orb), range(n_orb)):
-        h_loc += (U / float(n_orb)) * c_dag(spin_up,m) * c(spin_up,mm) * c_dag(spin_dn,mmm) * c(spin_dn,np.mod(m+mmm-mm,n_orb))
+        h_loc += (U / float(n_orb)) * get_c(spin_up, m, True) * get_c(spin_up, mm, False) \
+            * get_c(spin_dn, mmm, True) * get_c(spin_dn, np.mod(m+mmm-mm, n_orb), False)
     
     return h_loc.real
 
-# Setup problem and gf_struct for each inequivalent trimer cluster A, B
+# Setup problem and gf_struct for each inequivalent trimer cluster
 n_clusters = 2
 n_orb = 3
 spin_names = ['up','dn']
-gf_struct = [set_operator_structure(spin_names, n_orb, off_diag=True) for _ in range(n_clusters)]
 
 # Setup non-interacting Hamiltonian matrix on the lattice
 tg = 0.5
-nkx = 60
+nkx = 30
 h0_k = get_h0_k(tg=tg, nkx=nkx, spin_names=spin_names)
-# FIXME why is h0_k getting nans sometimes??
-#for bl in h0_k.keys():
-#    idx = np.where(np.isnan(h0_k[bl]))
-#    h0_k[bl][idx] = 0
 
 # Set up class to work out k-space integration weights
 beta = 40 # inverse temperature
 n_target = 8 # 2/3rds filling
 kweight = SmearingKWeight(beta=beta, n_target=n_target)
 
+# Set up gf_structure of clusters
+gf_struct_molecule = [('up_A', 1), ('up_E1', 1), ('up_E2', 1), ('dn_A', 1), ('dn_E1', 1), ('dn_E2', 1)]
+gf_struct_molecule_mapping = {'up_A':'up', 'up_E1':'up', 'up_E2':'up', 'dn_A':'dn', 'dn_E1':'dn', 'dn_E2':'dn'}
+gf_struct = [gf_struct_molecule for _ in range(n_clusters)]
+gf_struct_mapping = [gf_struct_molecule_mapping for _ in range(n_clusters)]
+
 # Make projectors onto each trimer cluster
-projectors = [dict(), dict()]
+projectors = [dict() for i in range(n_clusters)]
 for i in range(n_clusters):
-    for bl_sub, bl_sub_size in gf_struct[i]:
-        projectors[i][bl_sub] =  np.eye(n_clusters*n_orb)[i*bl_sub_size:(bl_sub_size+i*bl_sub_size),:]
+    projectors[i]['up_A'] =  np.eye(n_clusters*n_orb)[0+i*n_orb:1+i*n_orb, :]
+    projectors[i]['dn_A'] =  np.eye(n_clusters*n_orb)[0+i*n_orb:1+i*n_orb, :]
+    projectors[i]['up_E1'] =  np.eye(n_clusters*n_orb)[1+i*n_orb:2+i*n_orb, :]
+    projectors[i]['dn_E1'] =  np.eye(n_clusters*n_orb)[1+i*n_orb:2+i*n_orb, :]
+    projectors[i]['up_E2'] =  np.eye(n_clusters*n_orb)[2+i*n_orb:3+i*n_orb, :]
+    projectors[i]['dn_E2'] =  np.eye(n_clusters*n_orb)[2+i*n_orb:3+i*n_orb, :]
 
 # Get the non-interacting kinetic Hamiltonian matrix on the lattice
-h0_kin_k = get_h0_kin_k(h0_k, projectors)
+h0_kin_k = get_h0_kin_k(h0_k, projectors, gf_struct_mapping=gf_struct_mapping)
 
 # Get the non-interacting local operator terms
-h0_loc = [get_h0_loc(h0_k=h0_k, P=P) for P in projectors]
+h0_loc = [get_h0_loc(h0_k=h0_k, P=P, gf_struct_mapping=gf_struct_mapping[i]) for i,P in enumerate(projectors)]
 
 # Get the local interaction operator terms
-#U = 5
-U = 0
-h_int = [get_hubb_N(spin_names=spin_names, U=U) for _ in range(n_clusters)]
+U = 4
+h_int = [get_hubb_trimer(spin_names=spin_names, U=U) for i in range(n_clusters)]
 
 # Define the local Hamiltonian
 h_loc = [h0_loc[i] + h_int[i] for i in range(n_clusters)]
 
 # Set up embedding solvers 
-embedding = [EmbeddingAtomDiag(h_loc[i], gf_struct[i]) for i in range(n_clusters)]
-#embedding = [EmbeddingAtomDiag(h_loc[0], gf_struct[0])]
-#embedding.append( EmbeddingDummy(embedding[0]) )
+#embedding = [EmbeddingAtomDiag(h_loc[i], gf_struct[i]) for i in range(n_clusters)]
+embedding = [EmbeddingAtomDiag(h_loc[0], gf_struct[0])]
+for i in range(n_clusters-1):
+    embedding.append( EmbeddingDummy(embedding[0]) )
 
-def make_clusters_equal(A):
+def symmetries(A):
     n_clusters = len(A)
+    # Paramagnetic
     for i in range(n_clusters):
-        for bl in A[i]:
-            A[i][bl] = A[0]['up']
+        A[i]['up_A'] = 0.5 * (A[i]['up_A'] + A[i]['dn_A'])
+        A[i]['dn_A'] = A[i]['up_A']
+        A[i]['up_E1'] = 0.5 * (A[i]['up_E1'] + A[i]['dn_E1'])
+        A[i]['dn_E1'] = A[i]['up_E1']
+        A[i]['up_E2'] = 0.5 * (A[i]['up_E2'] + A[i]['dn_E2'])
+        A[i]['dn_E2'] = A[i]['up_E2']
+    # E1 = E2.conj()
+    for i in range(n_clusters):
+        A[i]['up_E2'] = A[i]['up_E1'].conj().T
+        A[i]['dn_E2'] = A[i]['dn_E1'].conj().T
     return A
-
-#from scipy.optimize import root as root_fun
 
 # Setup RISB solver class  
 S = LatticeSolver(h0_k=h0_kin_k,
@@ -145,31 +159,23 @@ S = LatticeSolver(h0_k=h0_kin_k,
                   embedding=embedding,
                   update_weights=kweight.update_weights,
                   projectors=projectors,
-                  #symmetries=[make_clusters_equal],
-                  force_real=True,
-                  #
-                  #root=root_fun,
-                  #return_x_new = False,
+                  symmetries=[symmetries],
+                  gf_struct_mapping=gf_struct_mapping,
 )
 
 # Solve
-#S.solve(tol=1e-6)
-#S.solve(tol=1e-6, method='linearmixing')
-
-x = S.solve(one_shot=True)
-x = S.solve(one_shot=True)
-x = S.solve(one_shot=True)
-x = S.solve(one_shot=True)
-x = S.solve(one_shot=True)
+S.solve(tol=1e-4)
+#for i in range(5):
+#    x = S.solve(one_shot=True)
  
 # Average number of particles on a cluster
 NOp = N_op(spin_names, n_orb, off_diag=True)
-N = [e.overlap(NOp) for e in embedding]
-
-# Effective total spin of a cluster
-S2Op = S2_op(spin_names, n_orb, off_diag=True)
-S2 = [e.overlap(S2Op) for e in embedding]
-
+#N = [e.overlap(NOp) for e in embedding]
+#
+## Effective total spin of a cluster
+#S2Op = S2_op(spin_names, n_orb, off_diag=True)
+#S2 = [e.overlap(S2Op) for e in embedding]
+#
 # Print out some interesting observables
 with np.printoptions(formatter={'float': '{: 0.4f}'.format}):
     for i in range(S.n_clusters):
@@ -178,6 +184,6 @@ with np.printoptions(formatter={'float': '{: 0.4f}'.format}):
             print(f"Quasiaprticle weight Z[{bl}] = \n{Z}")
         for bl, Lambda in S.Lambda[i].items():
             print(f"Correlation potential Lambda[{bl}] = \n{Lambda}")
-        print(f"Number of partices on cluster N = \n{N[i]:0.4f}")
-        print(f"Effective spin of cluster S = \n{(0.5 * np.sqrt(4 * (S2[i] + 1)) - 1):0.4f}")
-        print()
+#        print(f"Number of partices on cluster N = \n{N[i]:0.4f}")
+#        print(f"Effective spin of cluster S = \n{(0.5 * np.sqrt(4 * (S2[i] + 1)) - 1):0.4f}")
+#        print()
